80b2fed1e2a5e933122f856d4db3c459e94425ee
Abhinav <chabhinav.95@gmail.com>
2020-04-19 01:25:07 -0500

    Add

---
+++ server/springvuedemo/src/main/java/com/example/springvuedemo/SpringvuedemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springvuedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringvuedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringvuedemoApplication.class, args);
+	}
+
+}
---
+++ server/springvuedemo/src/test/java/com/example/springvuedemo/SpringvuedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springvuedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringvuedemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
